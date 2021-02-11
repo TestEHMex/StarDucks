01db31bfde5c0d2099671989c1a1acff75e4e064
TestEHMex <testehmex@gmail.com>
2021-02-11 14:42:47 -0500

    Updates

---
+++ precache-manifest.fa2ac8e354720d5117692b28c3acca1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88d2987c85d77f7339788a6d14c521f",
+    "url": "/StarDucks/index.html"
+  },
+  {
+    "revision": "4c8d78f5201f7530b0f3",
+    "url": "/StarDucks/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b0b27e94b8356a6564e9",
+    "url": "/StarDucks/static/js/2.8464f10d.chunk.js"
+  },
+  {
+    "revision": "4c8d78f5201f7530b0f3",
+    "url": "/StarDucks/static/js/main.39afedd8.chunk.js"
+  },
+  {
+    "revision": "4f00b50ef70992b9055e",
+    "url": "/StarDucks/static/js/runtime-main.18014260.js"
+  }
+]);
